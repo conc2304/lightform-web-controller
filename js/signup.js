bbee48979ddb637f3544372d6aa5675dc31b248d
Nathan Fischer <nathan@lightform.com>
2019-08-22 17:45:02 +0000

    Disable submit button for user signup after click
    https://lightforminc.atlassian.net/browse/CLOUD-94

--- js/signup.js
+++ js/signup.js
@@ -8,9 +8,13 @@ async function signup() {
 	var passwordInput = document.getElementById('password-input');
 	var password = passwordInput.value;
 
+	let submitButton = document.getElementById('submit');
+	submitButton.disabled = true;
+
 	passwordInput.classList.remove('is-invalid');
 
 	var response = await createUser(first, last, email, password);
+	submitButton.disabled = false;
 
 	if(!response.response.ok) {
 		warning.textContent = response.body.fields[0].message;
