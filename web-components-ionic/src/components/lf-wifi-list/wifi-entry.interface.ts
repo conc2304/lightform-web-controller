9b7d47228776f64f3f375cc14991abafff2267f4
Jose Conchello <joseconchello@joses-mbp.attlocal.net>
2020-09-10 16:13:57 -0500

    CREATOR-3804: remove Ionic web component test from top level

--- web-components-ionic/src/components/lf-wifi-list/wifi-entry.interface.ts
+++
@@ -1,7 +0,0 @@
-import { SignalStrength } from "./wifi-signal-strength.enum";
-
-export interface WifiEntry {
-    wifiName: string;
-    passwordProtected: boolean;
-    signalStrength: SignalStrength;
-}
