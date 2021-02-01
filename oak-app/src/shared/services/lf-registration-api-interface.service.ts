3b479698d89194c494e8ab31daa52234eaa6d927
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2021-02-01 16:59:45 -0600

    whatever

--- oak-app/src/shared/services/lf-registration-api-interface.service.ts
+++ oak-app/src/shared/services/lf-registration-api-interface.service.ts
@@ -16,9 +16,7 @@ class LfRegistrationApiInterface {
     this.log.warn('postRegistrationCode');
 
     // @ts-ignore Android
-    // const authToken = Android.getAuthToken();
-
-    const authToken = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMzA5MDQyMC00YTM5LTRmYmItYTIzYy0wMmQ5YjQ0YmFhYTgiLCJleHAiOjE2MTIyMjgyODgsIm5iZiI6MTYxMjIwNjY4OCwiaWF0IjoxNjEyMjA2Njg4LCJqdGkiOiI0NTU5IiwiaG1uIjp0cnVlfQ.uwIN-dY5NsmfjiO7ybIftv7fu2JkuADxrLxEXKdejw9uBEoDsPaq2O7VAYenUfuJFB7pvTBRxYJ3Am_Hi9r7uA';
+    const authToken = Android.getAuthToken();
 
     const response = await fetch(`${LfConf.apiUrl}/devices/me/registrationCode`, {
       method: 'PUT',
